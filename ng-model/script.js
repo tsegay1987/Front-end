7dc8d75e2b56ef5d199a3cfd4849fe1426ceecbb
memoryfraction <rong.fan1031@gmail.com>
2016-09-01 21:33:39 -0500

    ng-model function

---
+++ ng-model/script.js
@@ -0,0 +1,17 @@
+//Created by: Rong Fan
+//Email:rong.fan1031@gmail.com
+//2016-8-30
+var app=angular.module("app05",[]);
+
+
+app.controller("Controller1", function(){
+   this.Object=
+   {
+     name:"David Beckam",
+     email:"david@gmail.com",
+     phonemodel:"6S", 
+     status:"Positive",
+     purchasedate:"2015-1-1",
+   };
+   
+});
